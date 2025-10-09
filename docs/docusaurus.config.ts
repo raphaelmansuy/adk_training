b0ea2373b2308221893165e1c197c27891eb27eb
Raphaël MANSUY <raphael.mansuy@gmail.com>
2025-10-09 22:58:02 +0800

    feat: add PWA support and manifest for ADK Training Hub
    - Added @docusaurus/plugin-pwa dependency to enable PWA features.
    - Created manifest.json for the application with relevant metadata and icons.
    - Added multiple icon images for different resolutions to support the PWA.

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -207,6 +207,65 @@ const config: Config = {
         language: ['en'],
       },
     ],
+    [
+      '@docusaurus/plugin-pwa',
+      {
+        debug: true,
+        offlineModeActivationStrategies: [
+          'appInstalled',
+          'standalone',
+          'queryString',
+        ],
+        pwaHead: [
+          {
+            tagName: 'link',
+            rel: 'icon',
+            href: '/img/favicon.ico',
+          },
+          {
+            tagName: 'link',
+            rel: 'manifest',
+            href: '/manifest.json',
+          },
+          {
+            tagName: 'meta',
+            name: 'theme-color',
+            content: '#25c2a0',
+          },
+          {
+            tagName: 'meta',
+            name: 'apple-mobile-web-app-capable',
+            content: 'yes',
+          },
+          {
+            tagName: 'meta',
+            name: 'apple-mobile-web-app-status-bar-style',
+            content: '#000',
+          },
+          {
+            tagName: 'link',
+            rel: 'apple-touch-icon',
+            href: '/img/ADK-192.png',
+          },
+          {
+            tagName: 'link',
+            rel: 'mask-icon',
+            href: '/img/favicon.ico',
+            color: '#25c2a0',
+          },
+          {
+            tagName: 'meta',
+            name: 'msapplication-TileImage',
+            content: '/img/ADK-144.png',
+          },
+          {
+            tagName: 'meta',
+            name: 'msapplication-TileColor',
+            content: '#000',
+          },
+        ],
+      },
+    ],
     // TODO: Add Giscus plugin when available
     // TODO: Add social sharing plugin
   ],
